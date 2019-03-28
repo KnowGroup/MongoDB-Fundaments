a1b894901a3494833a0a7aaf7eb473a6a12cb78a
KnowGroup <45757509+KnowGroup@users.noreply.github.com>
2019-03-28 14:14:21 +0530

    Mongo Increment Update Operators
    Mongo Increment Update Operators

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Update users in the database', function () {
     let mowgli;
     
     beforeEach(function (done) {
-        mowgli = new User({ name  : 'Mowgli'});
+        mowgli = new User({ name  : 'Mowgli', postCount : 0});
         mowgli.save()
             .then(()=>{
                 assert(!mowgli.isNew);
@@ -55,5 +55,16 @@ describe('Update users in the database', function () {
                 User.findByIdAndUpdate(mowgli._id,{ name : 'Bagira'}),
                 done
         );
+    });
+    
+    it('A User can have their postcount incremented by 1', function (done) {
+        User.update({name : 'Mowgli'},{ $inc : {postCount:1}})
+                .then(() => User.findOne({name : 'Mowgli'}))
+                .then((user) => {
+                    console.log("User fetched form database : "+user.name);
+                    console.log("Mowgli Number of posts : "+user.postCount);
+                    assert(user.postCount === 1);
+                done();
+        });
     });
 });
